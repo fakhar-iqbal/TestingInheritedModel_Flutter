4e7293e232aaa14b3fb10fe27072f99c2ecc9cf8
Muhammad Fakhar-e-Iqbal Abdullah <fakhareiqbal3534@gmail.com>
2024-09-25 23:21:32 +0500

    inherited model

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-
+import 'dart:developer' as devtools show log;
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,12 +21,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var color1 = Colors.yellow;
+  var color2 = Colors.blue;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Hello World'),
       ),
+      body: AvailableColorsWidget(
+          color1: color1,
+          color2: color2,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  TextButton(
+                      onPressed: () {
+                        setState(() {
+                          color1 = colors.getRandomElement();
+                        });
+                      },
+                      child: const Text('Change color1')),
+                  TextButton(
+                      onPressed: () {
+                        setState(() {
+                          color2 = colors.getRandomElement();
+                        });
+                      },
+                      child: const Text('Change color2')),
+                ],     
+              ),
+              const ColorWidget(
+                color: AvailableColors.one,
+              ),
+              const ColorWidget(
+                color: AvailableColors.two,
+              ),
+            ],
+          )),
     );
   }
 }
@@ -34,8 +67,8 @@ class _HomePageState extends State<HomePage> {
 enum AvailableColors { one, two }
 
 class AvailableColorsWidget extends InheritedModel<AvailableColors> {
-  final AvailableColors color1;
-  final AvailableColors color2;
+  final MaterialColor color1;
+  final MaterialColor color2;
 
   const AvailableColorsWidget({
     super.key,
@@ -51,17 +84,30 @@ class AvailableColorsWidget extends InheritedModel<AvailableColors> {
       aspect: aspect,
     )!;
   }
-  
+
   @override
-  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
-    
-    throw UnimplementedError();
+  bool updateShouldNotify(covariant AvailableColorsWidget oldWidget) {
+    devtools.log('updateShouldNotify');
+    return color1 != oldWidget.color1 || color2 != oldWidget.color2;
   }
-  
+
   @override
-  bool updateShouldNotifyDependent(covariant InheritedModel<AvailableColors> oldWidget, Set<AvailableColors> dependencies) {
-    
-    throw UnimplementedError();
+  bool updateShouldNotifyDependent(
+    covariant AvailableColorsWidget oldWidget,
+    Set<AvailableColors> dependencies,
+  ) {
+    devtools.log('updateShouldNotifyDependent');
+    if (dependencies.contains(AvailableColors.one) &&
+        color1 != oldWidget.color1) {
+      return true;
+    }
+
+    if (dependencies.contains(AvailableColors.two) &&
+        color2 != oldWidget.color2) {
+      return true;
+    }
+
+    return false;
   }
 }
 
@@ -83,5 +129,26 @@ extension RandomElement<T> on Iterable<T> {
   T getRandomElement() => elementAt(Random().nextInt(length));
 }
 
+class ColorWidget extends StatelessWidget {
+  final AvailableColors color;
+  const ColorWidget({super.key, required this.color});
 
-// 16.11
+  @override
+  Widget build(BuildContext context) {
+    switch (color) {
+      case AvailableColors.one:
+        devtools.log('Color1 widget got rebuilt');
+        break;
+      case AvailableColors.two:
+        devtools.log('Color2 widget got rebuilt');
+        break;
+    }
+
+    final provider = AvailableColorsWidget.of(context, color);
+
+    return Container(
+      height: 100,
+      color: color == AvailableColors.one ? provider.color1 : provider.color2,
+    );
+  }
+}
